8beb4874511c2e02d6e5c666e06b8f6fcfa798de
cmtheforce <cmtheforce@gmail.com>
2016-06-07 16:23:49 +0000

    subject init

--- client/subjects/init/app.js
+++ client/subjects/init/app.js
@@ -1,2 +1,24 @@
 // define app module
 // define a run function
+
+//DICHIARO IL MODULO E LA FACCIO PARTIRE
+var app = angular.module("artooInit",[])
+                // STATO PRE RUN
+                .config(angular.noop)
+               /* .run(function(){
+                })*/
+                //MODO ALTERNATIVO 
+                .run(() => {
+                     console.info("I run in the setter!");
+                });
+//SI UTILIZZA IL SOTTOSTANTE
+//RICHIAMO IL MODULO
+angular.module("artooInit")
+       .run(() => {
+            console.info("I run in the getter!!");
+        });
+
+
+app.run(function(){
+    console.info("I run from the module in the var!!");
+});
